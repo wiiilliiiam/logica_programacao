9fc62ec6b6d42e007149a83dd420aa4f2f77e07e
william <williamfernando919@gmail.com>
2023-11-01 16:06:18 -0300

    c

--- lista_exercicios_1/q14.py
+++ lista_exercicios_1/q14.py
@@ -5,3 +5,10 @@ print("Hello World!")
 print("Hello" , "World!")
 
 # item c
+print("Hello!\n My name is John")
+
+# item d
+print("Hello!\t My name is John")
+
+# item e
+print("Um dia, ela disse: “Estarei aqui por você")
